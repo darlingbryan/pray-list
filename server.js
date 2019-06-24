c41f0da322da34e824c5d9bfc57f2a0b51cd652d
Bryan Gumpic <darlingbryan@gmail.com>
2019-06-24 08:42:56 -0400

    Connect Mongo DB. Setup User and Auth functionality

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+connectDB()
 
 app.use(express.json({ extended: false }))
 
@@ -9,7 +12,6 @@ app.get('/', (req, res) => {
 })
 
 //define routes
-
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 // app.use('api/prayers', require('./routes/prayers'))
