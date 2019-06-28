768ef125eb3568565f6cb30ce4f164deacae686e
Bryan Gumpic <darlingbryan@gmail.com>
2019-06-27 21:36:02 -0400

    Set up Navlinks when user is authenicated.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useContext, useEffect, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import AuthContext from '../../contexts/auth/authContext'
 
 const Navbar = ({ title, icon }) => {
+  const { isAuthenticated, loadUser, user, logoutUser } = useContext(
+    AuthContext
+  )
+
+  useEffect(() => {
+    loadUser()
+  }, [])
+
+  const onLogOut = () => {
+    logoutUser()
+  }
+
+  const NavLinks = () => {
+    if (isAuthenticated) {
+      return (
+        <Fragment>
+          <h3>Hi {user.name}</h3>
+          <i className='fas fa-sign-out-alt' onClick={onLogOut} />
+        </Fragment>
+      )
+    } else {
+      return (
+        <Fragment>
+          <Link to='/about' className='item'>
+            About
+          </Link>
+          <Link to='/register' className='item'>
+            Register
+          </Link>
+          <Link to='/login' className='item'>
+            Login
+          </Link>
+        </Fragment>
+      )
+    }
+  }
   return (
     <div className='ui text menu'>
       <Link to='/'>
@@ -12,15 +49,7 @@ const Navbar = ({ title, icon }) => {
       </Link>
 
       <div className='right menu'>
-        <Link to='/about' className='item'>
-          About
-        </Link>
-        <Link to='/register' className='item'>
-          Register
-        </Link>
-        <Link to='/login' className='item'>
-          Login
-        </Link>
+        <NavLinks />
       </div>
     </div>
   )
